c071ff8908985273d0c66b7f56f7efe9560cdafd
Matt <46137770+xMdb@users.noreply.github.com>
2022-01-16 20:46:29 +1000

    feat: add responsive design features for hero and nav

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,20 +1,62 @@
-import { Text, Link } from '@chakra-ui/react';
-import Image from 'next/image';
+import { Text, Link, VStack, SimpleGrid, Flex, Box } from '@chakra-ui/react';
 import { CTAButton } from '../components/CTAButton';
 
 export const Hero = () => {
 	return (
 		<>
-			<Text fontSize="5xl" fontWeight="black">
-				Expand your community with a <Text color="green.200">guild chat bridge.</Text>
-			</Text>
-			<Text fontSize="2xl" fontWeight="light" color="white">
-				Stay tuned for more information.
-			</Text>
-			<Link href="https://twitter.com/hychatmc" isExternal>
-				<CTAButton />
-			</Link>
-			<Image src="/discord.png" alt="Discord Example" width={430} height={240} />
+			{/* Desktop */}
+			<DesktopHero />
+
+			{/* Mobile */}
+			<MobileHero />
 		</>
+	);
+};
+
+const DesktopHero = () => {
+	return (
+		<Flex display={{ base: 'none', md: 'flex' }}>
+			<SimpleGrid columns={[2, null, 0]} spacing={100}>
+				<VStack w="full" h="full" py={[2, 4, 6, 8]} px={[50, 130, 196]} spacing={6} align="flex-start">
+					<Text fontSize={{ base: '4xl', md: '5xl', lg: '6xl' }} fontWeight="black">
+						Expand your community with a <Text color="green.200">guild chat bridge.</Text>
+					</Text>
+					<Text fontSize="2xl" fontWeight="light" color="white">
+						Stay tuned for more information.
+					</Text>
+					<Link href="https://twitter.com/hychatmc" isExternal>
+						<CTAButton />
+					</Link>
+				</VStack>
+				<VStack w="full" h="full" py={[2, 4, 6, 8]} px={196} spacing={6} align="flex-start">
+					{/* eslint-disable-next-line @next/next/no-img-element */}
+					<img src="/discord.png" alt="Discord Example" />
+				</VStack>
+			</SimpleGrid>
+		</Flex>
+	);
+};
+
+const MobileHero = () => {
+	return (
+		<Flex flex={{ base: 1, md: 'auto' }} ml={{ base: -2 }} display={{ base: 'flex', md: 'none' }}>
+			<Flex justifyContent="center" alignItems="center" flexDirection="column" p={30} pt={125} textAlign="center">
+				<Text fontSize="4xl" fontWeight="black">
+					Expand your community with a <Text color="green.200">guild chat bridge.</Text>
+				</Text>
+				<Text fontSize="2xl" fontWeight="light" color="white" py={30}>
+					Stay tuned for more information.
+				</Text>
+				<Box pb={10}>
+					<Link href="https://twitter.com/hychatmc" isExternal>
+						<CTAButton />
+					</Link>
+				</Box>
+				<Box pl={1}>
+					{/* eslint-disable-next-line @next/next/no-img-element */}
+					<img src="/discord.png" alt="Discord Example" />
+				</Box>
+			</Flex>
+		</Flex>
 	);
 };
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -6,67 +6,50 @@ import {
 	Button,
 	Stack,
 	Collapse,
-	Icon,
 	Link,
-	Popover,
-	PopoverTrigger,
-	PopoverContent,
-	useColorModeValue,
-	useBreakpointValue,
 	useDisclosure,
 } from '@chakra-ui/react';
-import { HamburgerIcon, CloseIcon, ChevronDownIcon } from '@chakra-ui/icons';
+import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons';
 
 export const Nav = () => {
 	const { isOpen, onToggle } = useDisclosure();
 	return (
 		<Box>
 			<Flex
-				color='white'
-				minH='60px'
+				color="white"
+				minH="60px"
 				py={2}
-				px={4}
+				px={[10, 130, 196]}
 				borderBottom={1}
-				borderStyle='solid'
-				borderColor='gray.200'
-				align='center'>
+				borderStyle="solid"
+				borderColor="gray.200"
+				align="center">
 				<Flex flex={{ base: 1, md: 'auto' }} ml={{ base: -2 }} display={{ base: 'flex', md: 'none' }}>
 					<IconButton
 						onClick={onToggle}
 						icon={isOpen ? <CloseIcon w={3} h={3} /> : <HamburgerIcon w={5} h={5} />}
-						variant='ghost'
-						aria-label='Toggle Navigation'
+						variant="ghost"
+						aria-label="Toggle Navigation"
+						mx={-1}
 					/>
 				</Flex>
 				<Flex flex={{ base: 1 }} justify={{ base: 'center', md: 'start' }}>
-					<Text
-						textAlign={useBreakpointValue({ base: 'center', md: 'left' })}
-						fontFamily={'heading'}
-						color={useColorModeValue('gray.800', 'white')}>
-						Logo
-					</Text>
-
+					{/* eslint-disable-next-line @next/next/no-img-element */}
+					<img src="/logo.png" alt="Hychat Logo" width={45} height={45} />
+					{/* <Text fontSize="lg" fontWeight="bold" ml={2}>
+						Hychat
+					</Text> */}
 					<Flex display={{ base: 'none', md: 'flex' }} ml={10}>
 						<DesktopNav />
 					</Flex>
 				</Flex>
 
 				<Stack flex={{ base: 1, md: 0 }} justify={'flex-end'} direction={'row'} spacing={6}>
-					<Button as={'a'} fontSize={'sm'} fontWeight={400} variant={'link'} href={'#'}>
-						Sign In
+					<Button colorScheme="green" variant="outline" display={{ base: 'flex', md: 'none' }}>
+						Login
 					</Button>
-					<Button
-						display={{ base: 'none', md: 'inline-flex' }}
-						fontSize={'sm'}
-						fontWeight="extrabold"
-						variant="outline"
-						color={'white'}
-						bg={'green.300'}
-						href={'#'}
-						_hover={{
-							bg: 'green.400',
-						}}>
-						Sign Up
+					<Button colorScheme="green" variant="outline" display={{ base: 'none', md: 'flex' }}>
+						Login to Dashboard
 					</Button>
 				</Stack>
 			</Flex>
@@ -79,40 +62,21 @@ export const Nav = () => {
 };
 
 const DesktopNav = () => {
-	const linkColor = useColorModeValue('gray.600', 'gray.200');
-	const linkHoverColor = useColorModeValue('gray.800', 'white');
-	const popoverContentBgColor = useColorModeValue('white', 'gray.800');
-
 	return (
 		<Stack direction={'row'} spacing={4}>
 			{NavItems.map((navItem) => (
-				<Box key={navItem.label}>
-					<Popover trigger={'hover'} placement={'bottom-start'}>
-						<PopoverTrigger>
-							<Link
-								p={2}
-								href={navItem.href ?? '#'}
-								fontSize={'sm'}
-								fontWeight={500}
-								color={linkColor}
-								_hover={{
-									textDecoration: 'none',
-									color: linkHoverColor,
-								}}>
-								{navItem.label}
-							</Link>
-						</PopoverTrigger>
-
-						{navItem.children && (
-							<PopoverContent
-								border={0}
-								boxShadow={'xl'}
-								bg={popoverContentBgColor}
-								p={4}
-								rounded={'xl'}
-								minW={'sm'}></PopoverContent>
-						)}
-					</Popover>
+				<Box >
+					<Link
+						p={5}
+						href={navItem.href ?? '#'}
+						fontSize="sm"
+						fontWeight="bold"
+						color="white"
+						_hover={{
+							color: 'green.200',
+						}}>
+						{navItem.label}
+					</Link>
 				</Box>
 			))}
 		</Stack>
@@ -120,7 +84,7 @@ const DesktopNav = () => {
 };
 const MobileNav = () => {
 	return (
-		<Stack bg={useColorModeValue('white', 'gray.800')} p={4} display={{ md: 'none' }}>
+		<Stack bg="gray.800" p={4} display={{ md: 'none' }}>
 			{NavItems.map((navItem) => (
 				<MobileNavItem key={navItem.label} {...navItem} />
 			))}
@@ -128,11 +92,9 @@ const MobileNav = () => {
 	);
 };
 
-const MobileNavItem = ({ label, children, href }: NavItem) => {
-	const { isOpen, onToggle } = useDisclosure();
-
+const MobileNavItem = ({ label, href }: NavItem) => {
 	return (
-		<Stack spacing={4} onClick={children && onToggle}>
+		<Stack spacing={4}>
 			<Flex
 				py={2}
 				as={Link}
@@ -142,44 +104,14 @@ const MobileNavItem = ({ label, children, href }: NavItem) => {
 				_hover={{
 					textDecoration: 'none',
 				}}>
-				<Text fontWeight={600} color={useColorModeValue('gray.600', 'gray.200')}>
-					{label}
-				</Text>
-				{children && (
-					<Icon
-						as={ChevronDownIcon}
-						transition={'all .25s ease-in-out'}
-						transform={isOpen ? 'rotate(180deg)' : ''}
-						w={6}
-						h={6}
-					/>
-				)}
+				<Text fontWeight={600}>{label}</Text>
 			</Flex>
-
-			<Collapse in={isOpen} animateOpacity style={{ marginTop: '0!important' }}>
-				<Stack
-					mt={2}
-					pl={4}
-					borderLeft={1}
-					borderStyle={'solid'}
-					borderColor={useColorModeValue('gray.200', 'gray.700')}
-					align={'start'}>
-					{children &&
-						children.map((child) => (
-							<Link key={child.label} py={2} href={child.href}>
-								{child.label}
-							</Link>
-						))}
-				</Stack>
-			</Collapse>
 		</Stack>
 	);
 };
 
 interface NavItem {
 	label: string;
-	subLabel?: string;
-	children?: Array<NavItem>;
 	href?: string;
 }
 
