fcb7c77eaf0bd6a14760c3581bcf5bb54a5536aa
Matt <46137770+xMdb@users.noreply.github.com>
2022-01-16 21:27:23 +1000

    style: change meta OG tags and resize discord img

--- src/components/Meta.tsx
+++ src/components/Meta.tsx
@@ -3,9 +3,10 @@ import Head from 'next/head';
 import { useRouter } from 'next/router';
 
 const metaConfig = {
-	title: 'Hychat',
-	description: "Easily create your Hypixel guild's new chat bot.", // eslint-disable-line quotes
+	title: "Easily create your Hypixel guild's new chat bot.", // eslint-disable-line quotes
+	description: 'Expand your community with a guild chat bridge.',
 	url: 'https://hych.at',
+	siteName: 'Hychat',
 };
 
 export const Meta = () => {
@@ -16,7 +17,7 @@ export const Meta = () => {
 			<Head>
 				<meta charSet="UTF-8" key="charset" />
 				<meta name="viewport" content="width=device-width,initial-scale=1" key="viewport" />
-				<link rel="apple-touch-icon" href={`${router.basePath}/apple-touch-icon.png`} key="apple" />
+				{/* <link rel="apple-touch-icon" href={`${router.basePath}/apple-touch-icon.png`} key="apple" />
 				<link
 					rel="icon"
 					type="image/png"
@@ -30,19 +31,23 @@ export const Meta = () => {
 					sizes="16x16"
 					href={`${router.basePath}/favicon-16x16.png`}
 					key="icon16"
-				/>
+				/> */}
 				<link rel="icon" href={`${router.basePath}/favicon.ico`} key="favicon" />
 			</Head>
 			<NextSeo
-				title={metaConfig.title}
+				title={`${metaConfig.title} - ${metaConfig.description}`}
 				description={metaConfig.description}
 				canonical={metaConfig.url}
 				openGraph={{
 					title: metaConfig.title,
 					description: metaConfig.description,
 					url: metaConfig.url,
-					locale: 'en_US',
-					site_name: `${metaConfig.title} - ${metaConfig.description}`,
+					site_name: metaConfig.siteName,
+				}}
+				twitter={{
+					handle: '@hychatmc',
+					site: '@hychatmc',
+					cardType: 'summary_large_image',
 				}}
 			/>
 		</>
